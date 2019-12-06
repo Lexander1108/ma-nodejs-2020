bece76a576f590018d64e2a80636688788f3ceb3
Alexander Kretov <alexander.kretov99@gmail.com>
2019-12-06 23:44:12 +0200

    completed task with promises and asyncs

--- task-4/src/callback.js
+++ task-4/src/callback.js
@@ -1,26 +1,25 @@
 function throwDice(min, max) {
-	const random = Math.floor(Math.random() * (max - min) + min);
-	if (random === 0) {
-		// console.log('Lost dice');
-		return Error('Lost dice ');
-	} else {
-		return random;
-	}
+ const random = Math.floor(Math.random() * (max - min) + min);
+ if (random === 0) {
+  // console.log('Lost dice');
+  return Error('Lost dice ');
+ }
+ return random;
 }
 
 let firstNumber;
 let secondNumber;
 
 setTimeout(() => {
-	firstNumber = throwDice(0, 6);
-	console.log(firstNumber);
+ firstNumber = throwDice(0, 6);
+ console.log(firstNumber);
 }, 700);
 
 setTimeout(() => {
-	secondNumber = throwDice(0, 6);
-	console.log(secondNumber);
+ secondNumber = throwDice(0, 6);
+ console.log(secondNumber);
 }, 2000);
 
 setTimeout(() => {
-	console.log(firstNumber + secondNumber);
+ console.log(firstNumber + secondNumber);
 }, 3000);
