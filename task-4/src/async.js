731c7c8345e4650330f971e55806948567fa9be9
Alexander Kretov <alexander.kretov99@gmail.com>
2019-12-07 17:24:48 +0200

    some changes

--- task-4/src/async.js
+++ task-4/src/async.js
@@ -4,11 +4,11 @@ const firstNumber = throwDiceAfter(700);
 const secondNumber = throwDiceAfter(2000);
 
 async function boot() {
-	const firstThrow = await firstNumber;
-	const secondThrow = await secondNumber;
-	setTimeout(() => {
-		console.log(firstThrow + secondThrow);
-	}, 3000);
+ const firstThrow = await firstNumber;
+ const secondThrow = await secondNumber;
+ setTimeout(() => {
+  console.log(firstThrow + secondThrow);
+ }, 3000);
 }
 
 boot();
