0ab0cb2fb06aaaf27a32d28b5dc3ea4f243816a9
Alexander Kretov <alexander.kretov99@gmail.com>
2019-12-07 18:51:25 +0200

    final upgrade

--- task-4/src/promise.js
+++ task-4/src/promise.js
@@ -1,34 +1,16 @@
-function throwDice(min, max) {
- return Math.floor(Math.random() * (max - min) + min);
-}
-
-function throwDiceAfter(ms) {
- return new Promise((resolve, reject) => {
-  setTimeout(() => {
-   const firstValue = throwDice(0, 6);
-   if (firstValue === 0) {
-    reject(new Error('Lost Dice'));
-   }
-   resolve(firstValue);
-  }, ms);
- });
-}
+const { throwDiceAfter } = require('./functions');
 
 const firstNumber = throwDiceAfter(700);
 const secondNumber = throwDiceAfter(2000);
 
 firstNumber
- .then((firstValue) => {
-  console.log(firstValue);
-  secondNumber
-   .then((secondValue) => {
-    console.log(secondValue);
-    setTimeout(() => {
-     console.log(firstValue + secondValue);
-    }, 3000);
-   })
-   .catch(console.error);
- })
- .catch(console.error);
-
-module.exports = throwDiceAfter;
+	.then((firstValue) => {
+		secondNumber
+			.then((secondValue) => {
+				setTimeout(() => {
+					console.log(firstValue + secondValue);
+				}, 3000);
+			})
+			.catch(console.error);
+	})
+	.catch(console.error);
