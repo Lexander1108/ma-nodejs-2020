9d38e2f6cac5c513531ea55a5247a99aa70f606d
Alexander Kretov <alexander.kretov99@gmail.com>
2019-12-07 18:51:38 +0200

    final upgrade

--- task-4/src/promise.js
+++ task-4/src/promise.js
@@ -4,13 +4,13 @@ const firstNumber = throwDiceAfter(700);
 const secondNumber = throwDiceAfter(2000);
 
 firstNumber
-	.then((firstValue) => {
-		secondNumber
-			.then((secondValue) => {
-				setTimeout(() => {
-					console.log(firstValue + secondValue);
-				}, 3000);
-			})
-			.catch(console.error);
-	})
-	.catch(console.error);
+ .then((firstValue) => {
+  secondNumber
+   .then((secondValue) => {
+    setTimeout(() => {
+     console.log(firstValue + secondValue);
+    }, 3000);
+   })
+   .catch(console.error);
+ })
+ .catch(console.error);
