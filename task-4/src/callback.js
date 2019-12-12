f8db3eabcc3e6dc03b86f16c849c22520168014d
Alexander Kretov <alexander.kretov99@gmail.com>
2019-12-12 15:03:38 +0200

    corrected mistakes

--- task-4/src/callback.js
+++ task-4/src/callback.js
@@ -1,25 +1,28 @@
-function throwDice(min, max) {
- const random = Math.floor(Math.random() * (max - min) + min);
- if (random === 0) {
-  // console.log('Lost dice');
-  return Error('Lost dice ');
- }
- return random;
+function getRandomNumber(min, max) {
+	const random = Math.floor(Math.random() * (max - min) + min);
+	if (random === 0) {
+		// console.log('Lost dice');
+		return Error('Lost dice ');
+	}
+	return random;
 }
 
-let firstNumber;
-let secondNumber;
+function throwDice(callback) {
+	const num = getRandomNumber(0, 6);
+	return callback(num);
+}
 
 setTimeout(() => {
- firstNumber = throwDice(0, 6);
- console.log(firstNumber);
+	throwDice((firstNumber) => {
+		console.log(firstNumber);
+		setTimeout(() => {
+			throwDice((secondNumber) => {
+				console.log(secondNumber);
+				setTimeout(() => {
+					const result = firstNumber + secondNumber;
+					console.log(result);
+				}, 1000);
+			});
+		}, 1300);
+	});
 }, 700);
-
-setTimeout(() => {
- secondNumber = throwDice(0, 6);
- console.log(secondNumber);
-}, 2000);
-
-setTimeout(() => {
- console.log(firstNumber + secondNumber);
-}, 3000);
