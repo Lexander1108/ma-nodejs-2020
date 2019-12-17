420222708ee711846fb7536968ad240039eaa61b
Alexander Kretov <alexander.kretov99@gmail.com>
2019-12-17 17:29:16 +0200

    done fifth homework

---
+++ task-5/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true
+	},
+	extends: [ 'airbnb-base', 'plugin:prettier/recommended' ],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	rules: {
+		semi: [ 'error', 'always' ],
+		quotes: [ 'error', 'single' ],
+		'no-console': 'off'
+	}
+};
