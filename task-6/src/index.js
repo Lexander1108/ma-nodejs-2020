a480357710c466cbab50011d63daf88aa9f07a38
Alexander Kretov <alexander.kretov99@gmail.com>
2020-01-09 16:11:43 +0200

    added env var

--- task-6/src/index.js
+++ task-6/src/index.js
@@ -1,13 +1,15 @@
 const os = require('os');
-
+const DEFAULT_RATE = 3000;
+process.env.RATE = process.env.RATE || DEFAULT_RATE;
+// console.log(process.env.RATE);
 setInterval(() => {
- const freeMemory = os.freemem();
- const totalMemory = os.totalmem();
- const filledMemory = totalMemory - freeMemory;
- console.log('Free memory: ', freeMemory);
- console.log('Total memory: ', totalMemory);
- console.log('Filled memory: ', filledMemory);
- setInterval(() => {
-  console.clear();
- }, 1000);
-}, 1000);
+	const freeMemory = os.freemem();
+	const totalMemory = os.totalmem();
+	const filledMemory = totalMemory - freeMemory;
+	console.log('Free memory: ', freeMemory);
+	console.log('Total memory: ', totalMemory);
+	console.log('Filled memory: ', filledMemory);
+	setInterval(() => {
+		console.clear();
+	}, process.env.RATE);
+}, process.env.RATE);
