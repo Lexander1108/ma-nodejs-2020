d5232cf998e34381110203c3f9fa2664b0ad7bb7
Alexander Kretov <alexander.kretov99@gmail.com>
2020-01-10 16:42:28 +0200

    little update

--- task-6/src/index.js
+++ task-6/src/index.js
@@ -14,53 +14,34 @@ const totalMemory = os.totalmem() / (1024 * 1024);
 const allocatedMemory = totalMemory - freeMemory;
 
 setInterval(() => {
- const lastFreeMemory = os.freemem() / (1024 * 1024);
- const lastTotalMemory = os.totalmem() / (1024 * 1024);
- const lastAllocatedMemory = lastTotalMemory - lastFreeMemory;
- const delta = lastAllocatedMemory - allocatedMemory;
- console.log('Total system memory:', lastTotalMemory.toFixed(3), 'MB');
- if (lastFreeMemory < DEFAULT_LIMIT && jsonParse === true) {
-  console.log('Free memory avaliable:', redColor, lastFreeMemory.toFixed(3), 'MB', resetColor);
- } else if (lastFreeMemory < DEFAULT_LIMIT && jsonParse === false) {
-  console.log('Free memory avaliable:', lastFreeMemory.toFixed(3), 'MB');
- } else {
-  console.log('Free memory avaliable:', lastFreeMemory.toFixed(3), 'MB');
- }
- console.log('Allocated memory:', lastAllocatedMemory.toFixed(3), 'MB');
+	console.clear();
+	const lastFreeMemory = os.freemem() / (1024 * 1024);
+	const lastTotalMemory = os.totalmem() / (1024 * 1024);
+	const lastAllocatedMemory = lastTotalMemory - lastFreeMemory;
+	const delta = lastAllocatedMemory - allocatedMemory;
+	console.log('Total system memory:', lastTotalMemory.toFixed(3), 'MB');
+	if (lastFreeMemory < DEFAULT_LIMIT && jsonParse === true) {
+		console.log('Free memory avaliable:', redColor, lastFreeMemory.toFixed(3), 'MB', resetColor);
+	} else if (lastFreeMemory < DEFAULT_LIMIT && jsonParse === false) {
+		console.log('Free memory avaliable:', lastFreeMemory.toFixed(3), 'MB');
+	} else {
+		console.log('Free memory avaliable:', lastFreeMemory.toFixed(3), 'MB');
+	}
+	console.log('Allocated memory:', lastAllocatedMemory.toFixed(3), 'MB');
 
- if (delta < 0 && jsonParse === true) {
-  console.log(
-   'Delta for previous allocated memory value:',
-   redColor,
-   delta.toFixed(3),
-   'MB',
-   resetColor,
-  );
- } else if (delta > 0 && jsonParse === true) {
-  console.log(
-   'Delta for previous allocated memory value:',
-   greenColor,
-   delta.toFixed(3),
-   'MB',
-   resetColor,
-  );
- } else if (delta < 0 && jsonParse === false) {
-  console.log('Delta for previous allocated memory value:', delta.toFixed(3), 'MB');
- } else {
-  console.log('Delta for previous allocated memory value:', delta.toFixed(3), 'MB');
- }
+	if (delta < 0 && jsonParse === true) {
+		console.log('Delta for previous allocated memory value:', redColor, delta.toFixed(3), 'MB', resetColor);
+	} else if (delta > 0 && jsonParse === true) {
+		console.log('Delta for previous allocated memory value:', greenColor, delta.toFixed(3), 'MB', resetColor);
+	} else if (delta < 0 && jsonParse === false) {
+		console.log('Delta for previous allocated memory value:', delta.toFixed(3), 'MB');
+	} else {
+		console.log('Delta for previous allocated memory value:', delta.toFixed(3), 'MB');
+	}
 
- if (freeMemory < DEFAULT_LIMIT && jsonParse === true) {
-  console.log(
-   redColor,
-   '!!! ATTENTION: Available memory is under the defined limit !!!',
-   resetColor,
-  );
- } else if (freeMemory < DEFAULT_LIMIT && jsonParse === false) {
-  console.log('!!! ATTENTION: Available memory is under the defined limit !!!');
- }
-
- setInterval(() => {
-  console.clear();
- }, DEFAULT_RATE);
+	if (freeMemory < DEFAULT_LIMIT && jsonParse === true) {
+		console.log(redColor, '!!! ATTENTION: Available memory is under the defined limit !!!', resetColor);
+	} else if (freeMemory < DEFAULT_LIMIT && jsonParse === false) {
+		console.log('!!! ATTENTION: Available memory is under the defined limit !!!');
+	}
 }, DEFAULT_RATE);
